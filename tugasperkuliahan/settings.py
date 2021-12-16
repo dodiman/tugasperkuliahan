79800e10cc4afe78f94c5555c379285df01cc5ed
dodiman <dodimantakimpoo@gmail.com>
2021-12-16 20:24:06 +0700

    +

--- tugasperkuliahan/settings.py
+++ tugasperkuliahan/settings.py
@@ -136,7 +136,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-ROOT_STATIC = os.path.join(BASE_DIR, 'staticfiles')
+ROOT_STATIC = os.path.join(BASE_DIR, 'static/assets')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')
 STATICFILES_DIRS = [
