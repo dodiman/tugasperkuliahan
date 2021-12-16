3c10bed15acbb7f52001bc581f6a6a8634a464b0
dodiman <dodimantakimpoo@gmail.com>
2021-12-16 18:38:28 +0700

    +

--- runserver.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo ini adalah file bat yang saya buat DODIMAN TAKIMPOO
-pause
-cd d:
-cmd /k "cd /python39/project/dua & venv\scripts\activate.bat & cd tugasperkuliahan & python manage.py runserver 0.0.0.0:8000"
